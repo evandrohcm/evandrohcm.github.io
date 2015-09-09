64705feb8570251456e28f66f0fb44481805dd84
Evandro Macedo <evandro.hcm@gmail.com>
2015-09-09 19:53:27 -0300

    Does a lot of shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,60 @@
 $(function(){
 
   /* --- Responsive nav menu --- */
-  var nav = $('#main-nav'),
+
+  var nav       = $('#main-nav'),
       navButton = $('#nav-button');
 
   navButton.on('click', toggleMenu);
 
   function toggleMenu () {
-    if (nav.is(":visible"))
-        nav.slideUp({
-          duration: 200,
-          complete: function () {
-            $(this).css('display','')
-          }
-        });
+
+    if (nav.is(":visible")) {
+
+      nav.slideUp({
+        duration: 200,
+        complete: function () {
+          $(this).css('display','')
+        }
+      });
+
+    }
     else
-        nav.slideDown(200);
+      nav.slideDown(200);
+
+  };
+
+
+  /* --- Load more button --- */
+
+  $("#load-more-button").click(loadMorePosts);
+
+  function loadMorePosts (event) {
+
+    event.preventDefault();
+
+    var _this          = this,
+        $blogContainer = $(".post-list"),
+        nextPage       = parseInt($blogContainer.attr("data-page")) + 1,
+        totalPages     = parseInt($blogContainer.attr("data-totalPages"));
+
+    $(this).addClass("loading");
+
+    $.get("/page" + nextPage, function (data) {
+
+      var htmlData  = $.parseHTML(data),
+          $articles = $(htmlData).find("article.post-item");
+
+      $blogContainer.attr("data-page", nextPage).append($articles);
+
+      if ($blogContainer.attr("data-totalPages") == nextPage) {
+        $(".load-more").remove();
+      }
+
+      $(_this).removeClass("loading");
+
+    });
+
   };
 
 });
